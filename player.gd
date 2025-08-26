9817c44bd3d6831f1889dfe8645b0e0ab39c2796
JMPriest <wenjun.zhu@proinnova.com.cn>
2025-08-26 13:21:52 +0800

    :fire: delete unused codes

--- player.gd
+++ player.gd
@@ -7,37 +7,6 @@ signal hit
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	
-#func _process(delta):
-	## The player's movement vector.
-	#var velocity = Vector2.ZERO
-	#if Input.is_action_pressed("move_right"):
-		#velocity.x += 1
-	#if Input.is_action_pressed("move_left"):
-		#velocity.x -= 1
-	#if Input.is_action_pressed("move_down"):
-		#velocity.y += 1
-	#if Input.is_action_pressed("move_up"):
-		#velocity.y -= 1
-#
-	#if velocity.length() > 0:
-		#velocity = velocity.normalized() * speed
-		#$AnimatedSprite2D.play()
-	#else:
-		#$AnimatedSprite2D.stop()
-	#
-	## new position for player
-	#position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
-	#
-	#if velocity.x != 0:
-		#$AnimatedSprite2D.animation = "walk"
-		#$AnimatedSprite2D.flip_v = false
-		## See the note below about the following boolean assignment.
-		#$AnimatedSprite2D.flip_h = velocity.x < 0
-	#elif velocity.y != 0:
-		#$AnimatedSprite2D.animation = "up"
-		#$AnimatedSprite2D.flip_v = velocity.y > 0
 		
 func _input(event: InputEvent) -> void:
 	if not event is InputEventMouseMotion:
